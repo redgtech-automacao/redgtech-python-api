11c41b09ef899afb6f8a90846758c5a7b24e1b80
Jonh Sady <jonh.system@gmail.com>
2025-02-04 13:48:40 -0300

    adjustments

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,21 +5,21 @@ from redgtech_api import RedgtechAPI
 
 @pytest.mark.asyncio
 async def test_get_data():
-    api = RedgtechAPI("pypi-AgEIcHlwaS5vcmcCJDVjYmNjY2NhLWI1MjctNGU2OS1hM2NlLTU5NDM0Mjg0YzNhYwACKlszLCJmYjQ2ZWMxNC04ZmUyLTRhMDQtOTJjYS0wZDdjOWZkYWExZjgiXQAABiCYSH-_FZq8qEKHUpXXbShUR1bGSqrhmRxzKBzbJ4fPZg")
+    api = RedgtechAPI("token")
     data = await api.get_data()
     assert "boards" in data
     await api.close()
 
 @pytest.mark.asyncio
 async def test_set_switch_state():
-    api = RedgtechAPI("pypi-AgEIcHlwaS5vcmcCJDVjYmNjY2NhLWI1MjctNGU2OS1hM2NlLTU5NDM0Mjg0YzNhYwACKlszLCJmYjQ2ZWMxNC04ZmUyLTRhMDQtOTJjYS0wZDdjOWZkYWExZjgiXQAABiCYSH-_FZq8qEKHUpXXbShUR1bGSqrhmRxzKBzbJ4fPZg")
+    api = RedgtechAPI("token")
     success = await api.set_switch_state("endpoint_id", True)
     assert success
     await api.close()
 
 @pytest.mark.asyncio
 async def test_set_light_brightness():
-    api = RedgtechAPI("pypi-AgEIcHlwaS5vcmcCJDVjYmNjY2NhLWI1MjctNGU2OS1hM2NlLTU5NDM0Mjg0YzNhYwACKlszLCJmYjQ2ZWMxNC04ZmUyLTRhMDQtOTJjYS0wZDdjOWZkYWExZjgiXQAABiCYSH-_FZq8qEKHUpXXbShUR1bGSqrhmRxzKBzbJ4fPZg")
+    api = RedgtechAPI("token")
     success = await api.set_light_brightness("endpoint_id", 128)
     assert success
     await api.close()
